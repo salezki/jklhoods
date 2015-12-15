d445afc56fc63e641dded018da3b67806398b61e
Sami-Peeter Kristian Kokkonen <sami.p.k.kokkonen@student.jyu.fi>
2015-12-15 14:38:25 +0200

    sql postit ja twiitit

--- instaposts.py
+++ instaposts.py
@@ -5,19 +5,19 @@ import urlparse
 import sys
 from flask import jsonify
 
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+con = psycopg2.connect(
+	database=url.path[1:],
+	user=url.username,
+	password=url.password,
+	host=url.hostname,
+	port=url.port
+)
+
 # Hae ensimmaista kertaa postit
 def instagramPosts():
-	con = None
 	try:
-		urlparse.uses_netloc.append("postgres")
-		url = urlparse.urlparse(os.environ['DATABASE_URL'])
-		con = psycopg2.connect(
-    		database=url.path[1:],
-    		user=url.username,
-    		password=url.password,
-    		host=url.hostname,
-    		port=url.port
-		)
 
 		data = []
 		cur = con.cursor()
@@ -29,110 +29,81 @@ def instagramPosts():
 			else:
 				continue
 		return jsonify(result=data)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
 			
 # Hakee uusimmat postit
 def fetchInstagram(shortcode):
-	con = None
 	try:
-		con = lite.connect('instagram.db')
 		posts = []
 		cur = con.cursor()
-		cur.execute('SELECT shortcode FROM instagram_posts WHERE id > (SELECT id FROM instagram_posts WHERE shortcode LIKE  ?)', (shortcode,))
+		cur.execute('SELECT shortcode FROM instagram_posts WHERE id > (SELECT id FROM instagram_posts WHERE shortcode LIKE  %s)', (shortcode,))
 		rows = cur.fetchall()
 		for row in rows:
 			posts.append([str(row[0])])
 		return jsonify(result=posts)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
 
 # Hakee uusimmat postit tagilla
 def fetchTagInstagram(req):
-	con = None
 	tagi = req["tagi"]
 	shortcode = req["shortcode"]
 	try:
-		con = lite.connect('instagram.db')
 		posts = []
 		cur = con.cursor()
-		cur.execute('SELECT instagram_posts.shortcode FROM instagram_posts, instagram_tags WHERE instagram_posts.mediaID = instagram_tags.mediaID AND instagram_tags.hashtag LIKE ? AND id > (SELECT id FROM instagram_posts WHERE shortcode LIKE  ?) ORDER BY id DESC LIMIT 10', (str(tagi), str(shortcode) ))
+		cur.execute('SELECT instagram_posts.shortcode FROM instagram_posts, instagram_tags WHERE instagram_posts.mediaID = instagram_tags.mediaID AND instagram_tags.hashtag LIKE %s AND id > (SELECT id FROM instagram_posts WHERE shortcode LIKE  %s) ORDER BY id DESC LIMIT 10', (str(tagi), str(shortcode) ))
 		rows = cur.fetchall()
 		for row in rows:
 			posts.append([str(row[0])])
 		return jsonify(result=posts)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
 
 # Hakee vanhempia posteja
 def fetchNext(shortcode):
-	con = None
 	try:
-		con = lite.connect('instagram.db')
 		posts = []
 		cur = con.cursor()
-		cur.execute('SELECT shortcode FROM instagram_posts WHERE id < (SELECT id FROM instagram_posts WHERE shortcode LIKE  ?) ORDER BY id DESC LIMIT 10', (shortcode,))
+		cur.execute('SELECT shortcode FROM instagram_posts WHERE id < (SELECT id FROM instagram_posts WHERE shortcode LIKE  %s) ORDER BY id DESC LIMIT 10', (shortcode,))
 		rows = cur.fetchall()
 		for row in rows:
 			posts.append([str(row[0])])
 		return jsonify(result=posts)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
 
 # Hae ensimmaista kertaa tagilla
 def hae_tagilla(req):
-	con = None
 	tagi = req["tagi"]
 
 	try:
-		con = lite.connect('instagram.db')
 		post = []
 		cur = con.cursor()
-		cur.execute('SELECT instagram_posts.shortcode FROM instagram_posts, instagram_tags WHERE instagram_posts.mediaID = instagram_tags.mediaID AND instagram_tags.hashtag LIKE ? ORDER BY id DESC LIMIT 10', (str(tagi), ))
+		cur.execute('SELECT instagram_posts.shortcode FROM instagram_posts, instagram_tags WHERE instagram_posts.mediaID = instagram_tags.mediaID AND instagram_tags.hashtag LIKE %s ORDER BY id DESC LIMIT 10', (str(tagi), ))
 		rows = cur.fetchall()
 		for row in rows:
 			post.append([str(row[0])])
 		return jsonify(result=post)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
 
 def haes_tagilla(req):
-	print req
-	con = None
 	tagi = req["tagi"]
 	shortcode = req["instacode"]
 	try:
-		con = lite.connect('instagram.db')
 		posts = []
 		cur = con.cursor()
-		cur.execute('SELECT instagram_posts.shortcode FROM instagram_posts, instagram_tags WHERE instagram_posts.mediaID = instagram_tags.mediaID AND instagram_tags.hashtag LIKE ? AND id < (SELECT id FROM instagram_posts WHERE shortcode LIKE  ?) ORDER BY id DESC LIMIT 10', (str(tagi), str(shortcode) ))
+		cur.execute('SELECT instagram_posts.shortcode FROM instagram_posts, instagram_tags WHERE instagram_posts.mediaID = instagram_tags.mediaID AND instagram_tags.hashtag LIKE %s AND id < (SELECT id FROM instagram_posts WHERE shortcode LIKE  %s) ORDER BY id DESC LIMIT 10', (str(tagi), str(shortcode) ))
 		rows = cur.fetchall()
 		for row in rows:
 			posts.append([str(row[0])])
 		return jsonify(result=posts)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
--- twiitit.py
+++ twiitit.py
@@ -5,22 +5,20 @@ import urlparse
 import sys
 from flask import jsonify
 
-def twiits():
-	con = None
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+con = psycopg2.connect(
+	database=url.path[1:],
+	user=url.username,
+	password=url.password,
+	host=url.hostname,
+	port=url.port
+)
 
+
+def twiits():
 	try:
 		#con = lite.connect('tweets.db')
-
-		urlparse.uses_netloc.append("postgres")
-		url = urlparse.urlparse(os.environ['DATABASE_URL'])
-		con = psycopg2.connect(
-			database=url.path[1:],
-			user=url.username,
-			password=url.password,
-			host=url.hostname,
-			port=url.port
-		)
-
 
 		tweets = []
 		cur = con.cursor()
@@ -31,108 +29,79 @@ def twiits():
 		for row in rows:
 			tweets.append([str(row[0])])
 		return jsonify(result=tweets)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
+
 
 def hae_tagilla(req):
-	con = None
 	tagi = req["tagi"]
 
 	try:
-		con = lite.connect('tweets.db')
-
 		tweets = []
 		cur = con.cursor()
-		cur.execute('SELECT twitter_tweets.tweetID FROM twitter_tweets, twitter_tags WHERE twitter_tweets.tweetID = twitter_tags.tweetID AND twitter_tags.hashtag LIKE ? ORDER BY id DESC LIMIT 10', (str(tagi), ))
+		cur.execute('SELECT twitter_tweets.tweetID FROM twitter_tweets, twitter_tags WHERE twitter_tweets.tweetID = twitter_tags.tweetID AND twitter_tags.hashtag LIKE %s ORDER BY id DESC LIMIT 10', (str(tagi), ))
 		rows = cur.fetchall()
 		for row in rows:
 			tweets.append([str(row[0])])
 		return jsonify(result=tweets)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except  e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
 
 def haes_tagilla(req):
-	con = None
 	tagi = req["tagi"]
 	tweetId = req["tweetId"]
 
 	try:
-		con = lite.connect('tweets.db')
-
 		tweets = []
 		cur = con.cursor()
-		cur.execute('SELECT twitter_tweets.tweetID FROM twitter_tweets, twitter_tags WHERE twitter_tweets.tweetID = twitter_tags.tweetID AND twitter_tags.hashtag LIKE ? AND twitter_tweets.tweetID < ? ORDER BY id DESC LIMIT 10', (str(tagi),tweetId))
+		cur.execute('SELECT twitter_tweets.tweetID FROM twitter_tweets, twitter_tags WHERE twitter_tweets.tweetID = twitter_tags.tweetID AND twitter_tags.hashtag LIKE %s AND twitter_tweets.tweetID < %s ORDER BY id DESC LIMIT 10', (str(tagi),tweetId))
 		rows = cur.fetchall()
 		for row in rows:
 			tweets.append([str(row[0])])
 		return jsonify(result=tweets)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
 
 def fetchTweets(tweetId):
-	con = None
 	try:
-		con = lite.connect('tweets.db')
 		data_tweet = []
 		cur = con.cursor()
-		cur.execute('SELECT tweetID FROM twitter_tweets WHERE tweetID > ?', (tweetId,))
+		cur.execute('SELECT tweetID FROM twitter_tweets WHERE tweetID > %s', (tweetId,))
 		rows = cur.fetchall()
 		for row in rows:
 			data_tweet.append([str(row[0])])
 		return jsonify(result=data_tweet)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
 
 def fetchTagTweets(req):
-	con = None
 	tagi = req["tagi"]
 	tweetId = req["tweetId"]
 	try:
-		con = lite.connect('tweets.db')
 		data_tweet = []
 		cur = con.cursor()
-		cur.execute('SELECT twitter_tweets.tweetID FROM twitter_tweets, twitter_tags WHERE twitter_tweets.tweetID = twitter_tags.tweetID AND twitter_tags.hashtag LIKE ? AND twitter_tweets.tweetID > ? ORDER BY id DESC LIMIT 10', (str(tagi), str(tweetId)) )
+		cur.execute('SELECT twitter_tweets.tweetID FROM twitter_tweets, twitter_tags WHERE twitter_tweets.tweetID = twitter_tags.tweetID AND twitter_tags.hashtag LIKE %s AND twitter_tweets.tweetID > %s ORDER BY id DESC LIMIT 10', (str(tagi), str(tweetId)) )
 		rows = cur.fetchall()
 		for row in rows:
 			data_tweet.append([str(row[0])])
 		return jsonify(result=data_tweet)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
 
 def haeSeuraavat(tweetId):
-	con = None
 	try:
-		con = lite.connect('tweets.db')
 		data_tweet = []
 		cur = con.cursor()
-		cur.execute('SELECT tweetID FROM twitter_tweets WHERE tweetID < ? LIMIT 10', (tweetId,))
+		cur.execute('SELECT tweetID FROM twitter_tweets WHERE tweetID < %s LIMIT 10', (tweetId,))
 		rows = cur.fetchall()
 		for row in rows:
 			data_tweet.append([str(row[0])])
 		return jsonify(result=data_tweet)
-	except lite.Error, e:
-		print "Error &s:" % e.args[0]
+	except e:
+		print e
 		sys.exit(1)
-	finally:
-		if con:
-			con.close()
